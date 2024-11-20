4a3eac584ffbb2b4cd4dc375a8bb07867817761d
gihhyeon <fineblue.kh@gmail.com>
2024-11-20 13:45:38 +0900

    plus dto

---
+++ src/main/java/com/kjm/Weather_wear/dto/ClothingResponseDTO.java
@@ -0,0 +1,12 @@
+package com.kjm.Weather_wear.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+public class ClothingResponseDTO {
+    private List<String> recommendedClothing; // 추천된 옷 리스트
+}
