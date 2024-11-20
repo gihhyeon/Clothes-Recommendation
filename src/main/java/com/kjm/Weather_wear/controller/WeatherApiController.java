923e575b2113d7ab8e4194099faefade3ed1c1e5
gihhyeon <fineblue.kh@gmail.com>
2024-11-20 10:10:54 +0900

    update-main

--- src/main/java/com/kjm/Weather_wear/controller/WeatherApiController.java
+++ src/main/java/com/kjm/Weather_wear/controller/WeatherApiController.java
@@ -14,10 +14,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.io.BufferedReader;
